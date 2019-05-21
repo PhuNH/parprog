5867876b8d7a12e7557f2728c61ef02bf87e5f7d
PhuNH <1079742+PhuNH@users.noreply.github.com>
2019-05-22 00:04:38 +0200

    Assignment 2 again: speedup 14.62?. Add new assignment 3

--- assignment2/student/mandelbrot_set_par.c
+++ assignment2/student/mandelbrot_set_par.c
@@ -7,37 +7,21 @@
 
 #include "mandelbrot_set.h"
 
-#define CHUNK_LEN 8
+#define CHUNK_LEN 16
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-
-struct data {
-	int* y_index;
-	int x_resolution;
-	int y_resolution;
-	int max_iter;
-	double view_x0;
-	double view_y1;
-	double x_stepsize;
-	double y_stepsize;
-	int palette_shift;
-	unsigned char ***image;
-};
+int * g_y_index;
+int g_x_resolution, g_y_resolution;
+int g_max_iter;
+double g_view_x0, g_view_y1;
+double g_x_stepsize, g_y_stepsize;
+int g_palette_shift;
+unsigned char ***g_image;
 
 void* work(void* args) {
-	struct data d = *(struct data*) args;
-	int* y_index = d.y_index;
-	int y_resolution = d.y_resolution;
-	int x_resolution = d.x_resolution;
-	int max_iter = d.max_iter;
-	double view_x0 = d.view_x0;
-	double view_y1 = d.view_y1;
-	double x_stepsize = d.x_stepsize;
-	double y_stepsize = d.y_stepsize;
-	int palette_shift = d.palette_shift;
-	unsigned char (*image)[x_resolution][3] = (unsigned char(*)[x_resolution][3])d.image;
-
-	double y;
+	int y_start, y_end;
+	
+    double y;
 	double x;
 
 	complex double Z;
@@ -45,26 +29,24 @@ void* work(void* args) {
 
 	int k;
 
-    int y_start, y_end;
-	
     for (;;) {
         pthread_mutex_lock(&mutex);
-        if (y_resolution - *y_index < 1) {
+        if (g_y_resolution - *g_y_index < 1) {
             pthread_mutex_unlock(&mutex);
             break;
         }
         
-        y_start = *y_index;
-        *y_index += CHUNK_LEN;
+        y_start = *g_y_index;
+        *g_y_index += CHUNK_LEN;
         pthread_mutex_unlock(&mutex);
         
-        y_end = y_resolution > y_start + CHUNK_LEN ? y_start + CHUNK_LEN : y_resolution;
+        y_end = g_y_resolution > y_start + CHUNK_LEN ? y_start + CHUNK_LEN : g_y_resolution;
         for (int i = y_start; i < y_end; i++)
         {
-            for (int j = 0; j < x_resolution; j++)
+            for (int j = 0; j < g_x_resolution; j++)
             {
-                y = view_y1 - i * y_stepsize;
-                x = view_x0 + j * x_stepsize;
+                y = g_view_y1 - i * g_y_stepsize;
+                x = g_view_x0 + j * g_x_stepsize;
 
                 Z = 0 + 0 * I;
                 C = x + y * I;
@@ -75,17 +57,17 @@ void* work(void* args) {
                 {
                     Z = Z * Z + C;
                     k++;
-                } while (cabs(Z) < 2 && k < max_iter);
+                } while (cabs(Z) < 2 && k < g_max_iter);
 
-                if (k == max_iter)
+                if (k == g_max_iter)
                 {
-                    memcpy(image[i][j], "\0\0\0", 3);
+                    memcpy(((unsigned char(*)[g_x_resolution][3])g_image)[i][j], "\0\0\0", 3);
                 }
                 else
                 {
-                    int index = (k + palette_shift)
+                    int index = (k + g_palette_shift)
                                 % (sizeof(colors) / sizeof(colors[0]));
-                    memcpy(image[i][j], colors[index], 3);
+                    memcpy(((unsigned char(*)[g_x_resolution][3])g_image)[i][j], colors[index], 3);
                 }
             }
         }
@@ -99,27 +81,24 @@ void mandelbrot_draw(int x_resolution, int y_resolution, int max_iter,
 						                double x_stepsize, double y_stepsize,
 									                int palette_shift, unsigned char (*image)[x_resolution][3],
 																	 int num_threads) {
-	pthread_t* threads = (pthread_t*) malloc(num_threads * sizeof(*threads));
-	struct data* d = (struct data*) malloc(num_threads * sizeof(*d));
-    int y_index = 0;
-	for (int t = 0; t < num_threads; t++) {
-		d[t].y_index = &y_index;
-		d[t].x_resolution = x_resolution;
-		d[t].y_resolution = y_resolution;
-		d[t].max_iter = max_iter;
-		d[t].view_x0 = view_x0;
-		d[t].view_y1 = view_y1;
-		d[t].x_stepsize = x_stepsize;
-		d[t].y_stepsize = y_stepsize;
-		d[t].palette_shift = palette_shift;
-		d[t].image = (unsigned char***)image;
-		pthread_create(&threads[t], NULL, work, d+t);
+	pthread_t threads[num_threads];
+	int y_index = 0;
+    g_y_index = &y_index;
+    g_x_resolution = x_resolution;
+    g_y_resolution = y_resolution;
+	g_max_iter = max_iter;
+    g_view_x0 = view_x0;
+    g_view_y1 = view_y1;
+    g_x_stepsize = x_stepsize;
+    g_y_stepsize = y_stepsize;
+    g_palette_shift = palette_shift;
+    g_image = (unsigned char***)image;
+    
+    for (int t = 0; t < num_threads; t++) {
+		pthread_create(&threads[t], NULL, work, NULL);
 	}
 
 	for (int t = 0; t < num_threads; t++) {
 		pthread_join(threads[t], NULL);
 	}
-
-	free(threads);
-	free(d);
 }
--- assignment3/unit_test.cpp
+++ assignment3/unit_test.cpp
@@ -1,42 +1,48 @@
-#include <boost/gil/extension/io/jpeg_dynamic_io.hpp>
 #include <iostream>
+#include <png++/png.hpp>
+#include <assert.h>
+#include"x_conv.h"
 
-#include"x_gradient.h"
-
-using namespace boost::gil;
-
+using namespace std;
 int main() {
-    rgb8_image_t img;
-    jpeg_read_image("tum.jpg",img);
 
-    gray8s_image_t img_out_ref(img.dimensions());
-    fill_pixels(view(img_out_ref),int8_t(0));
 
-    gray8s_image_t img_out(img.dimensions());
-    fill_pixels(view(img_out),int8_t(0));
 
-    for (int t = 2; t < 8; t++)
+    Matrix filter = getGaussian(13, 13, 1000);
+    Image image = loadImage("TUM.png");
+
+    Image newImage_ref;
+    Image newImage;
+
+    int height_ref,width_ref;
+    int height_par,width_par;
+
+    for (int t = 2; t < 33; t+=30)
     {
-        x_luminosity_gradient_ref(const_view(img), view(img_out_ref));
-        x_luminosity_gradient(const_view(img), view(img_out),t);
-        
-        auto v_ref = color_converted_view<gray8_pixel_t>(const_view(img_out_ref));
-        auto v = color_converted_view<gray8_pixel_t>(const_view(img_out));
+        newImage_ref = applyFilter_ref(&image, filter, t);
+        newImage     = applyFilter(&image, filter, t);
+         
+        height_par = newImage[0].size();   
+        width_par = newImage[0][0].size();
+        height_ref = newImage_ref[0].size();
+        width_ref = newImage_ref[0][0].size();
+        assert(height_par == height_ref);
+        assert(width_par == width_ref);
 
-        for (int y=0; y<v_ref.height(); ++y){
-            auto v_it = v.row_begin(y);
-            auto v_ref_it = v_ref.row_begin(y);
-            for (int x=0; x<v_ref.width(); ++x) {
-                if (*v_it !=  *v_ref_it){
-                    fprintf(stderr, "Computation with %d threads failed\n", t);
-                    fprintf(stderr, "Image differs from the one produced by the sequential version\n");
-                    return 1;
-                }
-                v_it++;
-                v_ref_it++;
-            }
-        }
+        //Compare images by each pixel
+
+	for (int i=0 ; i<height_ref ; i++) {
+            for (int j=0 ; j<width_ref ; j++) {
+		for (int d=0 ; d<3 ; d++) {
+		    if(newImage_ref[d][i][j] != newImage[d][i][j])
+		    {
+	   	       fprintf(stderr, "Computation with %d threads failed\n", t);
+                       fprintf(stderr, "Image differs from the one produced by the sequential version\n");
+                       return 1;
+		    }
+		}
+	    }
+	}
     }
-    
     return 0;
 }
