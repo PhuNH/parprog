f982a1d22723f745d8155c03397a8b01b2744738
PhuNH <1079742+PhuNH@users.noreply.github.com>
2019-06-03 00:41:19 +0200

    Assignment 4: speed up 9.56. With numThreads, 2 tasks, firstprivate and final on numThreads

--- assignment4/student/familytree_par.c
+++ assignment4/student/familytree_par.c
@@ -1,8 +1,33 @@
 #include "familytree.h"
+#include <omp.h>
+
+#define T 2
+
+int par_traverse(tree *node, int numThreads) {
+    if (node == NULL) return 0;
+    
+    numThreads /= 2;
+    int father_iq, mother_iq;
+    
+#pragma omp task shared(father_iq) firstprivate(numThreads) final(numThreads < T)
+    father_iq = par_traverse(node->father, numThreads);
+#pragma omp task shared(mother_iq) firstprivate(numThreads) final(numThreads < T)
+    mother_iq = par_traverse(node->mother, numThreads);
+    
+#pragma omp taskwait
+    node->IQ = compute_IQ(node->data, father_iq, mother_iq);
+    genius[node->id] = node->IQ;
+
+    return node->IQ;
+}
 
 int traverse(tree *node, int numThreads){
-	
-	// TODO implement your solution in here.
-
+    omp_set_num_threads(numThreads);
+#pragma omp parallel 
+    {
+#pragma omp single
+        par_traverse(node, numThreads);
+    }
+    
     return node->IQ;
 }
